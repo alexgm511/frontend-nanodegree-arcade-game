af34b9df325bf2de91e46ce57fe2d7a5fed16997
Alex G Mendoza <alexgm511@gmail.com>
2015-02-23 20:15:25 -0800

    Cleaned up the code to make better use of constructors, classes and superclasses and to add comments.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
-function getSpeed() {
-	// Use a random number to assign the lane and speed of enemy
-	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-	//return Math.random() * (max - min) + min;
-	var speed = Math.floor(Math.random() * (600-180)) + 180;
-	return speed;
-}
+	// Declare all variables that may be changed in future updates
+var myEnemySprite = 'images/enemy-bug.png';
+var myPlayerSprite = 'images/char-boy.png';
+var homeX = 2 * 101;
+var homeY = 5 * 83 - 25;
 
 function getLane() {
 	// Use random number from 1 to 3 to assign a lane to enemy
@@ -14,35 +12,58 @@ function getLane() {
 	return lane;
 }
 
-	//Agorithm to detect collision in 2D games from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+	//Algorithm to detect collision in 2D games from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
 function collision(pX, pY, eX, eY) {
-	var playerRect = {x: pX, y: pY, width: 68, height: 76}
-	var enemyRect = {x: eX, y: eY, width: 100, height: 68}
+	//pRect is the player, eRect is the enemy
+	var pRect = {x: pX, y: pY, width: 68, height: 76}
+	var eRect = {x: eX, y: eY, width: 100, height: 68}
 
-	if (playerRect.x < enemyRect.x + enemyRect.width &&
-	   playerRect.x + playerRect.width > enemyRect.x &&
-	   playerRect.y < enemyRect.y + enemyRect.height &&
-	   playerRect.height + playerRect.y > enemyRect.y) {
+	if (pRect.x < eRect.x + eRect.width &&
+	   pRect.x + pRect.width > eRect.x &&
+	   pRect.y < eRect.y + eRect.height &&
+	   pRect.height + pRect.y > eRect.y) {
 		// collision detected!
 		player.home();
 	}
 
 }
 
-// Enemies our player must avoid
-var Enemy = function() {
+	// Start with Sprite superclass with the 3 shared variables:
+	// 		the sprite image, and the x and y coordinates
+var Sprite = function(mySprite, myX, myY) {
+	this.sprite = mySprite;
+	this.x = myX;
+	this.y = myY;
+}
+
+Sprite.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+	// Enemies our player must avoid
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-	// Position enemy just outside the canvas
-	this.x = -101;
-	this.y = getLane();
-	this.speed = getSpeed();
+	
+	// Build out the Enemy subclass
+var Enemy = function(mySprite, myX, myY) {
+	Sprite.call(this, mySprite, myX, myY)
+};
 
+	// Create the Enemy prototype to define the Enemy functions
+Enemy.prototype = Object.create(Sprite.prototype);
+Enemy.prototype.constructor = Enemy;
+
+
+Enemy.prototype.mySpeed = function() {
+	// Use a random number to assign the lane and speed of enemy
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+	//return Math.random() * (max - min) + min;
+	this.speed = Math.floor(Math.random() * (600-180)) + 180;
 }
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -50,12 +71,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-	// If ouside the canvas send back to beginning, else move along
+	// If ouside the canvas send back to beginning, else continue moving
 	
 	if (this.x > 505 + 101) {
 		this.x = -101;
 		this.y = getLane();
-		this.speed = getSpeed();
+		this.mySpeed(); // = getSpeed();
 	} else {
 		collision(player.x + 30, player.y, this.x, this.y);
 		this.x += (this.speed * dt);
@@ -63,30 +84,27 @@ Enemy.prototype.update = function(dt) {
 }
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+/* 	Both the Enemy and the Player objects inherit
+	the render = function from Sprite superclass */
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
-	//player built following Enemy class provided
-	this.sprite = 'images/char-boy.png';
-	this.home();
-}
+
+	// Build out Player subclass
+var Player = function(mySprite, myX, myY) {
+	Sprite.call(this, mySprite, myX, myY)
+};
+
+Player.prototype = Object.create(Sprite.prototype);
+Player.prototype.constructor = Player;
+
 
 Player.prototype.update = function() {
 
 }
 
-	// Draw player on the screen
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);	
-}
-
 Player.prototype.handleInput = function(key) {
-
 	switch ( key ) {
 		case "left":
 			//move left
@@ -113,31 +131,29 @@ Player.prototype.handleInput = function(key) {
 				player.y += 83;
 			break;
 	}
-	//console.log('X= '+this.x);
-	//console.log('Y= '+this.y);
-
 }
 
 	// Function that returns player to home base
 Player.prototype.home = function() {
-	this.x = 2 * 101;
-	this.y = 5 * 83 - 25;
+	this.x = homeX;
+	this.y = homeY;
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies = [];
-var enemy1 = new Enemy();
-allEnemies.push(enemy1);
-var enemy2 = new Enemy();
-allEnemies.push(enemy2);
-var enemy3 = new Enemy();
-allEnemies.push(enemy3);
+// There are never more than 3 enemies on the screen
+// So only 3 enemies are needed.
+var enemy1, enemy2, enemy3;
+var allEnemies = [enemy1, enemy2, enemy3];
 
-var player = new Player();
+for (i=0; i < 3; i++) {
+	allEnemies[i] = new Enemy(myEnemySprite, -101, getLane());
+	allEnemies[i].mySpeed();
+}
 
+var player = new Player(myPlayerSprite, homeX, homeY);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
